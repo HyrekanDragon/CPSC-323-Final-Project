5462f8dc47e2c213c005c5f35873d23e09eb8873
Stratakent <45743399+Stratakent@users.noreply.github.com>
2018-12-11 23:39:30 -0800

    Update p3.cpp

--- p3.cpp
+++ p3.cpp
@@ -17,6 +17,7 @@ int main()
 	  if (line == "program")//beginning line
 	  {
 		  line = "#include <iostream>";//required to run
+		  cout << "Replacing program with #include <iostream>...\n";
 		  offile << line;//to put in the new cpp file.
 		  
 		  while (line != ";")//check to end at the semicolon originally in the file.
@@ -24,6 +25,7 @@ int main()
 				  infile >> line;
 				  if (line == ";") //skips it and hits the next line of code.
 				  {
+					  cout << "Removing ; for program...\n";
 					  offile << endl;
 					  break;
 				  }
@@ -33,24 +35,28 @@ int main()
     else if (line == "var")//var can become using namespace std.
 	  {
 		  line = "using namespace std;";
+	   	  cout << "Replacing var with using namespace std;..\n";
 		  offile << line << endl;
 	  }
 	  
 	else if (line == "integer" || line == ":integer" )//integer can become int. Alaso accounts for chance text is attached.
 	  {
 		  line = "int";
+		cout << "Replacing integer with int...\n";
 		  offile << line;
 	  }
     
   	else if (line == "begin")//begin can be int main() {
 	  {
 		  line = "int main()";
+		cout << "Replacing begin with int main() {...\n";
 		  offile << line << endl << "{" << endl;
 	  }
   	
 	else if (line == "show")//show works like cout.
 	{
 		line = "cout";
+		cout << "Replacing show and reformatting its content with cout... \n";
 		offile << line;
 		infile >> line;
 
@@ -74,13 +80,20 @@ int main()
 	  else if (line == "end")//end can become the right brace.
 	  {
 		  line = "}";
+		  cout << "Replacing end with a }...\n";
 		  offile << line << endl;
 	  }
 
 	  else if (line == ";")//tricky but every semicolon ends with a new line of code; easier readability.
+	  {
+		  cout << "shifting to the next line because of ;...\n";
 		  offile << line << endl;
+	  }
 
 	  else //other that continues on regardless.
+	  {
+		  cout << "Reading in " << line << "...\n";
 		  offile << line;
+	  }
    }
 }
